5d021ab15aff759339cde5f125fdd86b53de0a50
abuthaha <aziz.abuthaha@gmail.com>
2021-09-13 09:52:57 +0700

    exam

---
+++ js/script.js
@@ -0,0 +1,40 @@
+$(function () {
+
+  let validator = $('form.needs-validation').jbvalidator({
+      errorMessage: true,
+      successClass: true,
+      language: 'dist/lang/en.json'
+  });
+
+  //new custom validate methode
+  validator.validator.custom = function (el, event) {
+
+      if ($(el).is('[name=password]') && $(el).val().length < 5) {
+          return 'Your password is too weak.';
+      }
+
+      return '';
+  }
+
+  let validatorServerSide = $('form.validatorServerSide').jbvalidator({
+      errorMessage: true,
+      successClass: true,
+  });
+
+  //serverside
+  $(document).on('submit', '.validatorServerSide', function () {
+
+      $.ajax({
+          method: "get",
+          url: "test.json",
+          data: $(this).serialize(),
+          success: function (data) {
+              if (data.status === 'error') {
+                  validatorServerSide.errorTrigger($('[name=username]'), data.message);
+              }
+          }
+      })
+
+      return false;
+  });
+})
